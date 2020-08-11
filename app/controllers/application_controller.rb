99080f2af71b939a4af2c7504e4811f607c94547
fuu0516 <fuu05160@gmail.com>
2020-08-11 20:25:27 +0900

    add username form and validate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resouce)
     root_path   #login後のpathを設定
+  end
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :username, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 end
