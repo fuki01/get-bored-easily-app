5b10d8ecce9e60983f8a06de867a09d210cab578
fuu0516 <fuu05160@gmail.com>
2020-08-24 21:19:44 +0900

    add test POST #create

--- spec/requests/devise_users_spec.rb
+++ spec/requests/devise_users_spec.rb
@@ -1,10 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "DeviseUsers", type: :request do
-  describe "GET /devise_users" do
-    it "works! (now write some real specs)" do
-      get devise_users_index_path
-      expect(response).to have_http_status(200)
+  let(:user) { create(:user) }
+  let(:user_params) { attributes_for(:user)}
+  let(:invalid_user_params) { attributes_for(:user, username: "") }
+
+  describe "POST #create" do
+    before do
+    end
+    context 'パラメータが妥当な場合' do
+      it "リクエストが成功する事" do
+        post user_registration_path, params: { user: user_params }
+        expect(response.status).to eq 302
+      end
+      it "createが成功する事" do
+        expect do
+          post user_registration_path, params: { user: user_params }
+        end.to change(User, :count).by 1
+      end
+      it "リダイレクトされる事" do
+        post user_registration_path, params: { user: user_params }
+        expect(response).to redirect_to root_url
+      end
+    end
+
+    context 'パラメータが不当な場合' do
+      it "リクエストが成功する事" do
+        post user_registration_path, params: { user: invalid_user_params }
+        expect(response.status).to eq 200
+      end
+      it "createが失敗する事" do
+        expect do
+          post user_registration_path, params: { user: invalid_user_params }
+        end.to_not change(User, :count)
+      end
+      it "エラーが表される事" do
+        post user_registration_path, params: { user: invalid_user_params }
+        expect(response.body).to include 'Username が入力されていません。'
+      end
     end
   end
 end
