73dc07db72a7f3c175775a8bbbe3c554b49c79e1
fuu0516 <fuu05160@gmail.com>
2020-08-24 20:08:06 +0900

    add devise users requests

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Myapp
                            helper_specs: false,
                            routing_specs: false,
                            controller_specs: true,
-                           request_specs: false
+                           request_specs: true
           g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
