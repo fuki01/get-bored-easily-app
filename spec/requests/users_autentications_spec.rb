f189470e866002b13814219e1593c6e5c84bc6b9
fuu0516 <fuu05160@gmail.com>
2020-08-24 21:33:32 +0900

    add test GET #edit

--- spec/requests/users_autentications_spec.rb
+++ spec/requests/users_autentications_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "UsersAutentications", type: :request do
-  describe "GET /users_autentications" do
-    it "works! (now write some real specs)" do
-      get users_autentications_index_path
-      expect(response).to have_http_status(200)
+  let(:user) { create(:user) }
+
+  describe 'GET #edit' do
+    subject{ get edit_user_registration_path }
+
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+      
+      it "リクエストが成功する事" do
+        is_expected.to eq 200
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it "リダイレクトされる事" do
+        is_expected.to redirect_to new_user_session_path
+      end
     end
   end
 end
