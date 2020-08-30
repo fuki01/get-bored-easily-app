642786badd4b8679f0f296097d1346fabb45113e
fuu0516 <fuu05160@gmail.com>
2020-08-31 07:50:16 +0900

    add traget test

--- spec/factories/targets.rb
+++ spec/factories/targets.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :target do
-    
+    body{ "早く寝る"}
+    user_id{ 1 }
   end
 end
--- spec/models/target_spec.rb
+++ spec/models/target_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Target, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '無効' do
+    it "bodyが無ければ無効な状態である事" do
+      target = Target.new(
+        body: nil
+      )
+      target.valid?
+      expect(target.errors[:body]).to include("が入力されていません。")
+    end
+    it "user_idが無ければ無効な状態である事" do
+      target = Target.new(
+        user_id: nil
+      )
+      target.valid?
+      expect(target.errors[:user_id]).to include("が入力されていません。")
+    end
+  end
+  context '有効' do
+    it "user_idとbody有れば有効な状態である事" do
+      expect(FactoryBot.build(:target, user: FactoryBot.create(:user))).to be_valid
+    end
+  end
 end
