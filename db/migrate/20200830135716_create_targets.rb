ef82581b354cf237dede216e876a8779b76daf8b
fuu0516 <fuu05160@gmail.com>
2020-08-31 08:08:09 +0900

    add target migrate

--- db/migrate/20200830135716_create_targets.rb
+++ db/migrate/20200830135716_create_targets.rb
@@ -1,7 +1,8 @@
 class CreateTargets < ActiveRecord::Migration[5.2]
   def change
     create_table :targets do |t|
-
+      t.text    :body
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_165133) do
+ActiveRecord::Schema.define(version: 2020_08_30_135716) do
+
+  create_table "targets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_165133) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
