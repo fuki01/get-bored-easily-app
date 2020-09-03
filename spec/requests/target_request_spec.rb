763abb6fcaca6afa7f61e0a10ef5828240c9cf54
fuu0516 <fuu05160@gmail.com>
2020-09-03 09:31:13 +0900

    delete target request rspec

--- spec/requests/target_request_spec.rb
+++ spec/requests/target_request_spec.rb
@@ -1,26 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Targets", type: :request do
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/target/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/target/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/target/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
