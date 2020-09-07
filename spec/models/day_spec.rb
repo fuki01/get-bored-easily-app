f0c52f03c998e992a5390e4d1ecb4bd743ee2915
fuu0516 <fuu05160@gmail.com>
2020-09-07 14:41:18 +0900

    update day models rspec

--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Day, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '無効'do
+    it "possibleが無ければ無効" do
+      day = Day.new(
+        possible: nil
+      )
+      day.valid?
+      expect(day.errors[:possible]).to include("が入力されていません。")
+    end
+    it "countが無ければ無効" do
+      day = Day.new(
+        count: nil
+      )
+      day.valid?
+      expect(day.errors[:count]).to include("が入力されていません。")
+    end
+    it "target_idが無ければ無効" do
+      day = Day.new(
+        target_id: nil
+      )
+      day.valid?
+      expect(day.errors[:target_id]).to include("が入力されていません。")
+    end
+  end
 end
