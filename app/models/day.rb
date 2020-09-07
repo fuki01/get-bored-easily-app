5106797c216db05e7a35474096f3f274b07dd1e6
fuu0516 <fuu05160@gmail.com>
2020-09-07 13:31:33 +0900

    update day association

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,4 +2,5 @@ class Day < ApplicationRecord
   validates :count, presence: true
   validates :possible, presence: true
   validates :target_id, presence: true
+  belongs_to :target
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -2,4 +2,5 @@ class Target < ApplicationRecord
   validates :body, presence: true
   validates :user_id, presence: true
   belongs_to :user
+  has_one :day
 end
