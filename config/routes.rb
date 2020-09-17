d6dd8f1c6b3d9109b6d3d1a6118d4982dbc40213
fuu0516 <fuu05160@gmail.com>
2020-09-17 15:10:17 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
   resources :target
+  resources :day, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
