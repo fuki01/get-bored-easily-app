a796ad1fb59d634a0ad2d957f72829a309ef1cdd
fuu0516 <fuu05160@gmail.com>
2020-09-17 17:05:03 +0900

    update entryday rspec test

--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -23,5 +23,12 @@ RSpec.describe Day, type: :model do
       day.valid?
       expect(day.errors[:target_id]).to include("が入力されていません。")
     end
+    it "entrydayが無ければ無効" do
+      day = Day.new(
+        target_id: nil
+      )
+      day.valid?
+      expect(day.errors[:target_id]).to include("が入力されていません。")
+    end
   end
 end
