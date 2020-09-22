aec862a0650263480ce571c43e42d9f9b58d7233
fuu0516 <fuu05160@gmail.com>
2020-09-22 20:45:39 +0900

    update taraget view

--- app/helpers/target_helper.rb
+++ app/helpers/target_helper.rb
@@ -1,2 +1,14 @@
 module TargetHelper
+  def target_nil?
+    @user.target.nil?
+  end
+  def body
+    @user.target.body
+  end
+  def day_empty?
+    @user.target.day.empty?
+  end
+  def complete_today
+     @user.target.day.last.entryday.strftime("%Y%m%d").to_i != Time.now.strftime('%Y%m%d').to_i
+  end
 end
