6070632d31eab7fdf107cd66487b6c9fa5b981e2
fuu0516 <fuu05160@gmail.com>
2020-10-11 13:49:49 +0900

    update: schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_124911) do
+ActiveRecord::Schema.define(version: 2020_09_22_093512) do
 
   create_table "days", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "count"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_09_18_124911) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "entryday"
+  end
+
+  create_table "points", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "sum"
+    t.integer "target_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "targets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
