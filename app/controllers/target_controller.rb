f974dd7a42490fa7ecdf271315ac155f625a06a8
fuu0516 <fuu05160@gmail.com>
2020-10-12 20:49:13 +0900

    update: update to improve readabilit

--- app/controllers/target_controller.rb
+++ app/controllers/target_controller.rb
@@ -5,7 +5,7 @@ class TargetController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = user_find_set
     @day = Day.new
     @userDay = target_last_set
     @target = target_last_set
@@ -17,7 +17,7 @@ class TargetController < ApplicationController
   end
 
   def create
-    @user = User.find(current_user.id)
+    @user = user_find_set
     @target = @user.targets.new(target_params)
     @target.user_id = current_user.id
     if @target.save
@@ -28,7 +28,7 @@ class TargetController < ApplicationController
   end
 
   def destroy
-    @user = User.find(current_user.id)
+    @user = user_find_set
     if target_last_set.destroy
       redirect_to target_path, notice: '目標を削除しました。'
     else
@@ -37,12 +37,12 @@ class TargetController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = user_find_set
     @target = target_last_set
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = user_find_set
     if target_last_set.update(target_params)
       redirect_to "/target/#{current_user.id}", notice: '目標を設定しました。'
     else
@@ -55,6 +55,9 @@ class TargetController < ApplicationController
   end
   private
 
+  def user_find_set
+    User.find(current_user.id)
+  end
   def target_last_set
     @user.targets.last
   end
