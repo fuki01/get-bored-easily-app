65431d756490639e03fd875c81b19f71aa106301
fuu0516 <fuu05160@gmail.com>
2020-12-06 12:28:04 +0900

    update: ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
+  get 'user/:id' => "user#show", as: 'user_profile'
   get 'target/list' => 'target#list'
   get 'target/clear' => 'target#clear', as:'target_clear'
   resources :target
