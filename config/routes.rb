454fe7cc2bf2da24bfb48384786e60c544e9030c
fuu0516 <fuu05160@gmail.com>
2020-12-30 00:14:38 +0900

    add: フォロー機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'point/show'
   root 'home#top'
   devise_for :users
+  get 'users' => "user#index", as: 'user_list'
   get 'user/:id' => "user#show", as: 'user_profile'
   get 'target/list' => 'target#list'
   get 'target/clear' => 'target#clear', as:'target_clear'
   resources :target
+  resources :relationships, only: [:create, :destroy]
   resources :day, only: [:create, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,6 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create(id: 1,email: 'fuu05160@gmail.com',username: 'fuki', password: 'password',created_at: "2020-09-17 06:44:16")
-Target.create(id: 1, body: 'testdata', user_id: 1)
-Point.create(sum: "100", target_id: 1, User_id:1)
-Day.create(count: 2, target_id: 1, entryday: "2020-11-19")
-Point.create(sum: "200", target_id: 1, User_id:1)
-Day.create(count: 3, target_id: 1, entryday: "2020-11-21")
+User.create(id: 2,email: 'fuu051020@gmail.com',username: 'fuki', password: 'password',created_at: "2020-09-17 06:44:16")
+User.create(id: 3,email: 'fuu051603@gmail.com',username: 'fuki', password: 'password',created_at: "2020-09-17 06:44:16")
